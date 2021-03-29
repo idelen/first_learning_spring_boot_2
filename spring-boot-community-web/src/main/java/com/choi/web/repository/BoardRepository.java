5f9847aa7fb5e9d1d195c76292e61d2fc1fdf494
CJP Desk <ch.jackpot@gmail.com>
2021-03-29 14:45:18 +0900

    first commit

---
+++ spring-boot-community-web/src/main/java/com/choi/web/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.choi.web.repository;
+
+import com.choi.web.domain.Board;
+import com.choi.web.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Board findByUser(User user);
+}
